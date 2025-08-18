7f8125304e3e21387007a9d2de386b740748f9db
marzia <you@example.com>
2025-08-18 19:33:08 +0200

    add favicon and fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Pages
+
+import HomePage from "./pages/Homepage";
+
 function App() {
   return (
     <>
@@ -8,7 +12,7 @@ function App() {
           <Route>
             layou di default
             <Route>Pagina di 404</Route>
-            <Route>index</Route>
+            <Route index element={<HomePage />} />
             <Route>paina dei luoghi</Route>
             <Route>pagina del dettaglio</Route>
           </Route>
